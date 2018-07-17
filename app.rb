af97170dc17aae4338d564ce683288cb1821dd33
lalahang <lylahang086@gmail.com>
2018-07-17 19:07:17 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-end
+  get '/' do 
+    "Hello, World!"
+  end
+ 
+  get '/name' do 
+    "My name is___"
+  end
+  
+  get '/hometown' do
+  "my hometown is___"
+  end
+ 
+  get '/favorite-song' do
+    "My favorite song is___"
+  end
+
+end 
