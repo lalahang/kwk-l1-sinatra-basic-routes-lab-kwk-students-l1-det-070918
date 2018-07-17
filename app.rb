6d0fb341e16619b399e1efa1b6588f19f23d9f9a
lalahang <lylahang086@gmail.com>
2018-07-17 19:09:17 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,20 +1,6 @@
-require_relative 'config/environment'
 
-class App < Sinatra::Base
-  get '/' do 
-    "Hello, World!"
-  end
- 
-  get '/name' do 
-    "My name is___"
-  end
-  
-  get '/hometown' do
-  "my hometown is___"
-  end
- 
-  get '/favorite-song' do
-    "My favorite song is___"
-  end
+get '/hello/:id' do 
+  @name = params[:id]
+  "Hello #{@name}!"
 
-end 
+end
